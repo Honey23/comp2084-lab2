3cbea9cfeaa82bde847ef14378515bd93c87f67c
Honey23 <200374848@student.georgianc.on.ca>
2018-09-22 19:38:31 -0700

    Completing my Lab2

---
+++ Lab1/Controllers/ProductssController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductssController : Controller
+    {
+        // GET: Productss
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Productss/Details
+
+        public ActionResult details(string YouSelectedProduct)
+        {
+            ViewBag.YouSelectedProduct = YouSelectedProduct;
+
+            return View();
+        }
+    }
+}
